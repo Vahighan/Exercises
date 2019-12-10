ff4d69f7078f5c73d4518a9f2333e906076f2556
Vahighan <mj.grodon@gmail.com>
2019-12-10 15:17:58 +0100

    Ex711

---
+++ Exercises/Ex711.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+float a=10.9;
+float b=10.98;
+float c=10.999;
+
+if(a>c&&a>b){
+cout<<a<<endl;}
+if(b>c&&b>a){
+cout<<b<<endl;}
+if(c>b&&c>a){
+cout<<c<<endl;}
+
+
+};
